1e186cb23f32216300fedae6c7ba28445f7d1fee
Bryan <bryanribero@hotmail.com>
2026-06-06 22:54:40 -0300

    creando el test para la implementacion de la funcion de hashRefreshToken

---
+++ src/__test__/functionHashRefreshToken.test.js
@@ -0,0 +1,35 @@
+import sequelize from '../db/database.js'
+import User from '../db/models/User.js'
+import { hashRefreshToken } from '../services/auth.service.js'
+import crypto from 'crypto'
+
+afterEach(async () => {
+  await User.destroy({
+    where: {},
+    force: true,
+  })
+})
+
+afterAll(async () => {
+  await sequelize.close()
+})
+
+describe('hashRefreshToken', () => {
+  it('debería hashear un refresh token correctamente', () => {
+    const refreshToken = 'tokenCorrecto'
+
+    const hashedToken = hashRefreshToken(refreshToken)
+
+    expect(crypto.createHash('sha256').update(refreshToken).digest('hex')).toBe(
+      hashedToken
+    )
+  })
+
+  it('debería lanzar un error si refreshToken es undefined', () => {
+    expect(() => hashRefreshToken()).toThrow()
+  })
+
+  it('debería lanzar un error si refreshToken no es un String', () => {
+    expect(() => hashRefreshToken(122)).toThrow()
+  })
+})
