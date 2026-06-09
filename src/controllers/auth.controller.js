edcd89be238dd8d77fb6632434d34c2d13c7785f
Bryan <bryanribero@hotmail.com>
2026-06-09 03:37:51 -0300

    Ajuste a la documentacion de swagger para agregar el nuevo endpoint de logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import { loginUser, registerNewUser } from '../services/auth/auth.service.js'
+import { revokedOldRefreshToken } from '../services/auth/utils/tokens.utils.js'
 
 export async function registerController(req, res, next) {
   try {
@@ -23,6 +24,19 @@ export async function loginController(req, res, next) {
       message: 'Inicio de sesión exitoso',
       accessToken: user.accessToken,
       refreshToken: user.refreshToken,
+    })
+  } catch (err) {
+    next(err)
+  }
+}
+
+export async function logoutController(req, res, next) {
+  try {
+    await revokedOldRefreshToken(req.user.id_user)
+
+    res.status(200).json({
+      success: true,
+      message: 'Sesión cerrada correctamente',
     })
   } catch (err) {
     next(err)
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import {
   loginController,
+  logoutController,
   registerController,
 } from '../controllers/auth.controller.js'
 import { credentials } from '../validators/credentialsAuth.js'
 import { validateFields } from '../middlewares/validateFields.js'
 import { loginRateLimit, registerRateLimit } from '../middlewares/rate.js'
+import { verifyAccessToken } from '../middlewares/verifyAccessToken.js'
 
 const router = Router()
 
@@ -24,5 +26,7 @@ router.post(
   validateFields,
   loginController
 )
+
+router.post('/logout', verifyAccessToken, logoutController)
 
 export default router
