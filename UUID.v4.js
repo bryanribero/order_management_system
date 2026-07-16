ae7baaeed6622bc8133755bf75a120838541cf48
Bryan <bryanribero@hotmail.com>
2026-07-16 04:20:14 -0300

    creacion de la conexion con la base de datos de produccion

--- UUID.v4.js
+++ UUID.v4.js
@@ -1,1 +1,5 @@
+import crypto from 'crypto'
+
 console.log(crypto.randomUUID())
+
+console.log(crypto.randomBytes(16).toString('hex'))
--- src/db/database.js
+++ src/db/database.js
@@ -9,16 +9,28 @@ dotenv.config({
   path: `.env.${env}`,
 })
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    dialect: 'postgres',
-    logging: false,
-  }
-)
+const sequelize =
+  env === 'prod'
+    ? new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        logging: false,
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
+      })
+    : new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASS,
+        {
+          host: process.env.DB_HOST,
+          port: Number(process.env.DB_PORT),
+          dialect: 'postgres',
+          logging: false,
+        }
+      )
 
 export default sequelize
